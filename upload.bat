b3717babe356569b6215cea5d83cbf0f49627363
jianming.mi <jianming.mi@tinno.com>
2023-03-07 21:27:22 +0800

    update

--- upload.bat
+++ upload.bat
@@ -1,12 +1,3 @@
-git fetch origin hexo_noise_theme:refs/remotes/origin/hexo_noise_theme
-
-git add -A
-git commit --no-edit -m "update"
-
-git rebase origin/hexo_noise_theme
-
-git push origin HEAD:refs/heads/hexo_noise_theme
-
 hexo clean && hexo generate && hexo deploy
 
 pause
