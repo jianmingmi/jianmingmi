6b2a8a87c024dedf202a6b1d2642b808b0f0cd8a
jianming.mi <jianming.mi@tinno.com>
2023-03-07 17:45:14 +0800

    update

---
+++ push.bat
@@ -0,0 +1,10 @@
+git fetch origin hexo_noise_theme:refs/remotes/origin/hexo_noise_theme
+
+git add -A
+git commit --no-edit -m "update"
+
+git rebase origin/hexo_noise_theme
+
+git push origin HEAD:refs/heads/hexo_noise_theme
+
+pause
